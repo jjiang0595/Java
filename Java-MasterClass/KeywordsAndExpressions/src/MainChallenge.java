14f8b61c097fc4bda7969ca0e686d1ca1abf5ebd
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-11 23:35:32 -0500

    Update MainChallenge.java

--- Java-MasterClass/KeywordsAndExpressions/src/MainChallenge.java
+++ Java-MasterClass/KeywordsAndExpressions/src/MainChallenge.java
@@ -1,15 +1,17 @@
 public class MainChallenge {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
+        int highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println(highScore);
+    }
 
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
+        return finalScore;
+
     }
-}
+
