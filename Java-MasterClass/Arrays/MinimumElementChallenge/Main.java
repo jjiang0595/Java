ffbc3c7c31393427be91446972cab8f0592504e2
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-16 21:00:41 -0500

    Minimum Element Challenge

---
+++ Java-MasterClass/Arrays/MinimumElementChallenge/Main.java
@@ -0,0 +1,39 @@
+package MinimumElementChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+        int minNum = findMin(returnedArray);
+        System.out.println("THE MIN NUM IS " + minNum);
+    }
+
+    public static int[] readIntegers() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter list of numbers");
+        String numList = s.nextLine();
+
+        String[] newNumList = numList.split(",");
+        int[] values = new int[newNumList.length];
+
+        for (int i = 0; i < newNumList.length; i++) {
+            values[i] = Integer.parseInt(newNumList[i].trim());
+        }
+        return values;
+    }
+
+    public static int findMin(int[] array) {
+        int temp = array[0];
+
+        for (int num : array) {
+            if (num < temp) {
+                temp = num;
+            }
+        }
+        return temp;
+    }
+}
