d0ddcdf1455037759aae0e3a989a69e214d545f8
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-17 23:41:59 -0500

    Autoboxing/Unboxing

---
+++ Java-MasterClass/List/Autoboxing/Main.java
@@ -0,0 +1,51 @@
+package Autoboxing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+
+        System.out.println(wrapperArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd', 'e'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var ourList = getList(1, 2, 3,4, 5);
+        System.out.println(ourList);
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer returnAnInt(int i) {
+        return i;
+    }
+
+    private static Double getDoubleObject() {
+        return Double.valueOf(100.00);
+    }
+
+    private static double getLiteralDoublePrimitive() {
+        return 100.0;
+    }
+}
